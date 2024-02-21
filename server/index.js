189e59cd1756d37d06bd045570c5a3e7c65e7777
aashish649 <aashishanand649@gmail.com>
2024-02-22 04:53:15 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   credentials: true,
-  origin:'http://localhost:3000',
+  origin:true,
 }));
 app.use(cookieParser());
 app.use(express.json());
